2324b9cc5e2db9b3e7c933b2acd740836a1615a9
Sanjiwani Sharma <sanjiwanisharma17@gmail.com>
2025-08-24 09:13:44 +0530

    Add files via upload

---
+++ Beginner Level/Numbers/2b.py
@@ -0,0 +1,7 @@
+radius = 84
+pi = 3.14
+pond_area = int(pi * radius ** 2)
+total_water = pond_area * 1.4
+
+print("Area of the pond without decimal point:", pond_area)
+print("Total water in the pond (liters) without decimal point:", int(total_water))
---
+++ Beginner Level/Numbers/2c.py
@@ -0,0 +1,4 @@
+distance = 490
+time = 7 * 60  # seconds
+speed = distance / time
+print("Speed in meters per second without decimal point:", int(speed))
