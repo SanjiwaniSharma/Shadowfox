98d80a6abb3ef3decc18b2765d0cea4afdb97e6a
Sanjiwani Sharma <sanjiwanisharma17@gmail.com>
2025-08-24 09:10:23 +0530

    Add files via upload

---
+++ Beginner Level/If Condition/4c.py
@@ -0,0 +1,15 @@
+australia = ["Sydney", "Melbourne", "Brisbane", "Perth"]
+uae = ["Dubai", "Abu Dhabi", "Sharjah", "Ajman"]
+india = ["Mumbai", "Bangalore", "Chennai", "Delhi"]
+
+city1 = input("Enter the first city: ")
+city2 = input("Enter the second city: ")
+
+if city1 in australia and city2 in australia:
+    print("Both cities are in Australia")
+elif city1 in uae and city2 in uae:
+    print("Both cities are in UAE")
+elif city1 in india and city2 in india:
+    print("Both cities are in India")
+else:
+    print("They don't belong to the same country")
