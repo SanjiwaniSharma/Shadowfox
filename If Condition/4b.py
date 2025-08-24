aa9b85a722feab37946af4cbb298eca3f2b7c1bc
Sanjiwani Sharma <sanjiwanisharma17@gmail.com>
2025-08-24 09:05:36 +0530

    Add files via upload

---
+++ If Condition/4b.py
@@ -0,0 +1,14 @@
+australia = ["Sydney", "Melbourne", "Brisbane", "Perth"]
+uae = ["Dubai", "Abu Dhabi", "Sharjah", "Ajman"]
+india = ["Mumbai", "Bangalore", "Chennai", "Delhi"]
+
+city = input("Enter a city name: ")
+
+if city in australia:
+    print(f"{city} is in Australia")
+elif city in uae:
+    print(f"{city} is in UAE")
+elif city in india:
+    print(f"{city} is in India")
+else:
+    print(f"{city} is not in the list")
